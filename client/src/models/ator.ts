c8e5c3b250f767dc102bd750a698ea89f81a97c9
frederico-enzo <peiter.frederico@gmail.com>
2026-01-29 11:56:12 -0300

    feat: add geradores and bess

--- client/src/models/ator.ts
+++ client/src/models/ator.ts
@@ -1,3 +1,7 @@
 export class Ator{  
     id!: number;
+    nome!: string;
+    telefone!: string;
+    email!: string;
+    cnpj_cpf!: string;
 }   
--- client/src/models/bess.ts
+++ client/src/models/bess.ts
@@ -1,4 +1,4 @@
 export class Bess{
     id!: number;
-
+    
 }
